db86189b9532add5f8d91dd0c65149c01452bcc7
MateuszPospiech <pospiechmateusz@gmail.com>
2019-06-23 23:50:06 +0200

    New project - PospiechIT in React

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import '../index.css';
+
+class Header extends React.Component {
+
+    render() {
+        return ( 
+            <div className="app container">
+                <div className="row">
+                  Menu
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Header;
