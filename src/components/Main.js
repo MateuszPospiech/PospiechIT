0916db812f7193a553feeaf544bc83d05af49f89
MateuszPospiech <pospiechmateusz@gmail.com>
2019-06-26 23:17:58 +0200

    Eddit logo path

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,8 @@ import SectionH2 from './SectionH2';
 
 import '../index.css';
 
+import logo from '../img/img-test.png';
+
 class Main extends React.Component {
 
     render() {
@@ -11,9 +13,9 @@ class Main extends React.Component {
             <main className="container-fluid main-bg">
                 <h2>Main</h2>
                 <Section/>
-                <SectionH2 title="Indywidualne podejście do klienta" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, maiores. Quibusdam amet mollitia reiciendis ullam vero sapiente enim ratione iusto sed atque quae commodi necessitatibus fugit nemo, aut quaerat esse." image="/static/media/img-test.31ce7f49.png" name="Indywidualne podejście" />
-                <SectionH2 title="Strony i sklepy internetowe" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, maiores. Quibusdam amet mollitia reiciendis ullam vero sapiente enim ratione iusto sed atque quae commodi necessitatibus fugit nemo, aut quaerat esse." image="/static/media/img-test.31ce7f49.png" name="Indywidualne podejście" />
-                <SectionH2 title="Aplikacje oraz skrypty" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, maiores. Quibusdam amet mollitia reiciendis ullam vero sapiente enim ratione iusto sed atque quae commodi necessitatibus fugit nemo, aut quaerat esse." image="/static/media/img-test.31ce7f49.png" name="Indywidualne podejście" />
+                <SectionH2 title="Indywidualne podejście do klienta" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, maiores. Quibusdam amet mollitia reiciendis ullam vero sapiente enim ratione iusto sed atque quae commodi necessitatibus fugit nemo, aut quaerat esse." image={logo} name="Indywidualne podejście" />
+                <SectionH2 title="Strony i sklepy internetowe" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, maiores. Quibusdam amet mollitia reiciendis ullam vero sapiente enim ratione iusto sed atque quae commodi necessitatibus fugit nemo, aut quaerat esse." image={logo} name="Indywidualne podejście" />
+                <SectionH2 title="Aplikacje oraz skrypty" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, maiores. Quibusdam amet mollitia reiciendis ullam vero sapiente enim ratione iusto sed atque quae commodi necessitatibus fugit nemo, aut quaerat esse." image={logo} name="Indywidualne podejście" />
             </main>
         )
     }
