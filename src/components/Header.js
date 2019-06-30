4731a7d65e4f0de6bd28e5b8556b9e4bbff3f08d
MateuszPospiech <pospiechmateusz@gmail.com>
2019-07-01 00:01:19 +0200

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,17 @@ import React from 'react';
 import '../index.css';
 
 class Header extends React.Component {
-
     render() {
         return ( 
             <header className="container-fluid">
-                <h2>Header</h2>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-3">
+                            <h1>Pośpiech.IT</h1>
+                        </div>
+                        <div>Menu</div>
+                    </div>
+                </div>
             </header>
         )
     }
